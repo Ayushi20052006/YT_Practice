8f94ed4336c61564e9f5ec64a7ed3ee6ab9dca38
Ayushi <ayushi.jha20052006@gmail.com>
2026-02-14 11:48:04 +0530

    Numbers and Math

---
+++ Basics_JS/03_nums_and_maths.js
@@ -0,0 +1,62 @@
+/*
+const num=57;
+console.log(num);
+
+const balance=new Number(100);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+const decimal=234.895;
+console.log(decimal.toPrecision(4));
+
+const hundred=10000000;
+console.log(hundred.toLocaleString());
+console.log(hundred.toLocaleString('en-IN'));
+*/
+
+//OUTPUTS:
+// 57
+// [Number: 100]
+// 100
+// 3
+// 234.9
+// 10,000,000
+// 1,00,00,000
+
+
+//=================MATHS====================
+/*
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.6));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.7));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(16));
+*/
+
+//OUTPUTS:
+// 5
+// 5
+// 4
+// 5
+// 4
+// 8
+// 4
+
+/*
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+const max=20;
+const min=10;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+*/
+
+//OUTPUTS:
+// 0.21918762583847873
+// 6.56609703318747
+// 2
+// 18

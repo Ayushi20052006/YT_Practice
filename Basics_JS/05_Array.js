11d7ac41ce0576cf0d7f3569b258c1e04cb1884b
Ayushi <ayushi.jha20052006@gmail.com>
2026-02-15 15:35:32 +0530

    Array basics

---
+++ Basics_JS/05_Array.js
@@ -0,0 +1,90 @@
+//Size of array in js is not fixed, we can add or remove elements from array and it will automatically adjust its size.
+//Zero based indexing
+//Shallow copy and deep copy in array
+
+
+/*
+const myArray =[0,1,2,3,4,5];
+const heroes=["IronMan","Captain America","Thor","Hulk"];
+
+console.log(myArray);
+console.log(heroes);
+console.log(typeof myArray);
+console.log(myArray.length);
+console.log(myArray[0]);
+console.log(myArray[6]);
+*/
+
+//OUTPUTS:
+// [ 0, 1, 2, 3, 4, 5 ]
+// [ 'IronMan', 'Captain America', 'Thor', 'Hulk' ]
+// object
+// 6
+// 0
+// undefined
+
+//Array Methods
+const myArray =[0,1,2,3,4,5];
+myArray.push(6);
+console.log(myArray);
+
+// [
+//   0, 1, 2, 3,
+//   4, 5, 6
+// ]
+
+myArray.pop();
+console.log(myArray);
+
+// [ 0, 1, 2, 3, 4, 5 ]
+
+myArray.unshift(-1);
+console.log(myArray);
+
+// [
+//   -1, 0, 1, 2,
+//    3, 4, 5
+// ]
+
+myArray.shift();
+console.log(myArray);
+
+// [ 0, 1, 2, 3, 4, 5 ]
+
+myArray.splice(2,1); //removes 1 element at index 2
+myArray.splice(2,0,10); //adds 10 at index 2
+myArray.splice(2,1,20); //replaces element at index 2 with 20
+console.log(myArray);
+ 
+// [ 0, 1, 20, 3, 4, 5 ]
+
+const arr=[7,8,9,10,11,12,13,14,15];
+const newArr=arr.slice(2,5); //returns a new array from index 2 to 4 (5 is not included)
+console.log('A',newArr);
+
+// A [ 9, 10, 11 ]
+
+console.log(myArray.indexOf(3)); //returns index of first occurrence of 3 else -1
+
+// 3
+
+console.log(myArray.includes(4)); //returns true if 4 is present in array else false
+
+// true
+
+const strArry =myArray.join(); //converts array to string with default separator as comma
+console.log(strArry);
+
+// 0,1,20,3,4,5
+
+const strArry2 =myArray.join("-"); //converts array to string with separator as -
+
+console.log(strArry2);
+
+// 0-1-20-3-4-5
+
+const str="Ayushi";
+const charArray =str.split(""); //converts string to array of characters
+console.log(charArray);
+
+// [ 'A', 'y', 'u', 's', 'h', 'i' ]

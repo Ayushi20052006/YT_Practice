0f01edde899574b65f723b09ad924d95cbed0cd1
Ayushi <ayushi.jha20052006@gmail.com>
2026-02-28 11:46:34 +0530

    if-else, switch, truthy-falsy value, nullish coalescing operator

--- Basics_JS/08_ControlFlow.js
+++ Basics_JS/08_ControlFlow.js
@@ -39,8 +39,13 @@ switch(day){
         console.log("Invalid day number.");
 }
 
+//TERNARY OPERATOR  => [condition ? expressionIfTrue : expressionIfFalse]
+let age=18;
+let canVote=age>=18?"You are eligible to vote.":"You are not eligible to vote.";
+console.log(canVote);
 
-//tpo check if the array is empty 
+
+//to check if the array is empty 
 let emptyArr=[]
 if(emptyArr.length===0){
     console.log("array is empty");
@@ -58,3 +63,42 @@ if(Object.keys(emptyObj).length===0){
 else{
     console.log("The object is not empty.");
 } 
+
+//false=0==""=null=undefined=NaN;  
+//all these values are considered falsy in JavaScript
+
+//true=1=="1"=true;
+//all these values are considered truthy in JavaScript
+
+
+
+//NULLISH COALESCING OPERATOR (??)  
+
+// KEYWORDS=> (null, undefined)
+// is used to provide a default value when dealing with null or undefined values. 
+// It returns the right-hand operand when the left-hand operand is null or undefined, 
+// otherwise it returns the left-hand operand.
+let val1;
+val1=5??10; //val1 will be 5 because it is not null or undefined
+console.log(val1);
+
+let val2=null;
+val2=6??10; //val2 will be 10 because it is null
+console.log(val2);
+
+let val3;
+val3 =null??10; //val3 will be 10 because it is null
+console.log(val3);
+
+let val4;
+val4=undefined??10; //val4 will be 10 because it is undefined
+console.log(val4);
+
+let val5;
+val5=0??10; //val5 will be 0 because it is not null or undefined
+console.log(val5);
+
+let val6;
+val6=""??10; //val6 will be "" because it is not null or undefined
+console.log(val6);
+
